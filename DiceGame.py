4dd979c9c24d7ae07278e5b58b24aad787b0606e
NewCavalry <DesireeGirottoDesign@gmail.com>
2023-01-17 15:14:27 -0500

    created determineWinner function to get the winner and created getPlayerResults function to get the results

--- DiceGame.py
+++ DiceGame.py
@@ -8,27 +8,29 @@ def askHowManyUsers(userNumberofPlayersAllowed): #userNumberofPlayers is an argu
     return howManyUsers
 
 def askUserName():
-    userName = input ("What is your name?: ")
+    userName = input ("what is your name?: ")
     return userName
 
 def greetUser(userName):
     print("Hi", userName)
 
+# the goal of this function is to populate a list of: player names & player totals based on the input passed into from total number of players and returns both lists
 def getPlayerResults(totalNumberOfPlayers):
-    playerNamesList = []
+    playerNamesList = []  #this is the list that stores all player names
     for currentUserNumber in range(1, totalNumberOfPlayers + 1):
-        print("Player", currentUserNumber,)
+        print("Player", str(currentUserNumber)+", ", end="")
         currentPlayerName = askUserName()
         playerNamesList.append(currentPlayerName)
 
+    userDiceTotalsList = []
     for currentNameIndex in range(totalNumberOfPlayers):
         userInput = input(playerNamesList [currentNameIndex] + " Hit enter to Roll")
         currentUserDieValueList = rollDice()
         currentUserDieTotal = getSumOfRollDice(currentUserDieValueList)
-        print(currentUserDieValueList)
-
-    return playerNamesList
-
+        userDiceTotalsList.append(currentUserDieTotal)
+        # print("Great Job! Your roll is:", currentUserDieValueList, "\nYour total is:", currentUserDieTotal)
+        # print("Great Job! Your roll is:", currentUserDieValueList)
+    return playerNamesList, userDiceTotalsList
 
 def rollDice(): #defining a function
     numberOfDice = 6
@@ -47,6 +49,22 @@ def getSumOfRollDice(userDieValueList): #userDieValueList is a parameter (placeh
         sumOfRollDice = sumOfRollDice + currentDieValue 
     return sumOfRollDice
 # 
+def determineWinner(playerNames, playerTotals):
+    # numberOfPlayers = len(playerNames)
+
+    # get highest totals from player total list
+    winningNumber = max(playerTotals)
+
+    # find index of highest score from player total list
+    winningNumberIndex = playerTotals.index(winningNumber)
+
+    # use index to display the corresponding name from names list
+    winnerName = playerNames [winningNumberIndex]
+
+    # print out the name along with the total for the winner
+    print("Congratulations!", winnerName, "you won with a total of:", winningNumber)
+
+
 def main():
     numberOfPlayersAllowed = 6
     printWelcomeMessage() #don't forget parenthesis to call the function
@@ -56,8 +74,8 @@ def main():
         revisedNumberofUsers = int(input ("sorry, the maximum number of players is " + str (numberOfPlayersAllowed) + ", please enter again: "))
         howManyUsersInMain = revisedNumberofUsers
         
-    playerNamesListinMain = getPlayerResults(howManyUsersInMain)
-    print (playerNamesListinMain)
+    playerNamesListinMain, playersTotalsListinMain = getPlayerResults(howManyUsersInMain)
+    determineWinner(playerNamesListinMain, playersTotalsListinMain)
 
 main() #calls the function
 
